cee2c8adc829c66e688089070cb6c79460a585de
Andrewminai24 <andrewminai24@gmail.com>
2020-09-09 15:48:38 -0700

    labs

--- Lab1/part1.cpp
+++ Lab1/part1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-   /*
+   
     cout << "Hello World";
     cout << endl;
     cout << "Hello"<< "World\n";
@@ -22,8 +22,19 @@ int main(){
     cout << "The new value of x is " << x;
     x = x+1;
     cout << "The final value of x is: " << x;
-*/
+
    
-    cout << "This is a backslash:\"\\\" "<< endl;
+    cout << "This is a backslash:\t\"\\\" "<< endl;
+
+     int b=10;
+    
+    // Write code here that multiplies x by 4 and displays the result.
+    // The asterisk will perform the multiplication operation: *
+    cout << b*4 << endl;
+    
+    
+    // Write code here that divides the result by 2.  The slash, /, will 
+    // perform division.
+    cout << b*4/2 << endl;
     return 0;
 }
---
+++ Lab2/part2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char letter;
+ char ch1,ch2,ch3,ch4,ch5,ch6,ch7;
+ ch1 = 'D';
+ cout << ch1 ;
+ ch2 = 114;
+ cout << ch2;
+ ch3 = '.';
+ cout << ch3;
+ ch4 =66;
+ cout << ch4;
+ ch5 ='o';
+ cout << ch5;
+ ch6=98;
+ cout << ch6;
+ string name = "Dr.Bob";
+ cout << name << endl; 
+ return 0;
+}
+
+
+
+
+
+
