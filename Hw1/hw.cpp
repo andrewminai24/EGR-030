899507b56d8e70906d8742c64cbcd136a1881d9c
Andrewminai24 <andrewminai24@gmail.com>
2020-09-09 14:37:24 -0700

    lab 1

---
+++ Hw1/hw.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int radius;
+float area;
+float PI=3.1415927;
+  
+radius=6;
+area=PI*radius*radius;
+  
+cout<<"Area of the circle: "<<area<<" for radius: "<<radius;
+
+return 0;
+}
---
+++ Lab1/day1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   /*
+    cout << "Hello World";
+    cout << endl;
+    cout << "Hello"<< "World\n";
+    cout << "Hello";
+    cout << "World\n";
+
+    cout << "My first name is: ";
+    cout << "Andrew" << endl;   
+    cout << endl << "My last name is: ";
+    cout << "Minai" << endl;
+    // ADD CODE HERE TO DISPLAY YOUR LAST NAME.
+    int x = 15;
+    cout << "The current value of x is: " << x;
+
+    x = 16;
+    cout << "The new value of x is " << x;
+    x = x+1;
+    cout << "The final value of x is: " << x;
+*/
+   
+    cout << "This is a backslash:\"\\\" "<< endl;
+    return 0;
+}
