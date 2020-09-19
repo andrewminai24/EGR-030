6480761012b821cba1ab3c30d1aa632ade229337
Andrewminai24 <andrewminai24@gmail.com>
2020-09-18 23:43:30 -0700

    lab

--- Lab3/part6.cpp
+++ Lab3/part6.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
+    char char1 =87;
+    char char2 = 68;
+    char char3 = 45;
+    char char4 = 40;
+
+
+    cout << char1 << char2 << char3 << static_cast<int>(char4) << endl;
+
+
+
     return(0);
 }
---
+++ Lab3/practice.cpp
@@ -0,0 +1,15 @@
+ #include<iostream>
+ using namespace std;
+  
+ int main(){
+
+ 
+int y,x;
+x=2;
+x > 3 ? y=4 : y =2;
+y==2 ? y=3 : y = 1;
+cout << y << endl;
+return (0);
+ }
+
+
