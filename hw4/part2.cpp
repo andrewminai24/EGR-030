ae8fbaeb2697b358cbb5561025acf6c141ed2d90
Andrewminai24 <andrewminai24@gmail.com>
2020-09-28 02:26:44 -0700

    labs

---
+++ hw4/part2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void){
+int x = 4;
+
+ if (x < 6)
+ {
+ cout << "x is less than 6\n";
+ }
+ cout << "Have a great day.\n";
+ return(0);
+}
---
+++ hw4/part3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(void){
+int x = 8;
+if (x < 6)
+ {
+ if (x > 2)
+ cout << "x is less than 6\n";
+ }
+ else
+ cout << "Have a great day.\n";
+}
+/*
+int x = 8;
+if (x < 6)
+ if (x > 2)
+ else
+    cout << "Have a great day.\n";
+ 
+return(0);
+}
+*/
