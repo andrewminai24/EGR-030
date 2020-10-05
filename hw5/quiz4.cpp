04c52a5c61e66aaf86416719b6d76927f45d1fb8
Andrewminai24 <andrewminai24@gmail.com>
2020-10-05 01:33:07 -0700

    labs and hw

---
+++ hw5/quiz4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main (void)
+{
+
+int x=7;
+
+if (x > 2)
+
+  {
+
+       if (x < 4)
+
+          cout << "x is less than 4\n";
+
+  }
+
+       else{
+
+          cout << "Have a great day.\n";
+
+       }
+cout << x << endl;
+return(0);
+}
